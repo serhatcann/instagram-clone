7f0a9750892e16b6f21af7020069a2370130a2fa
Serhat Can <serhatcan1996@gmail.com>
2022-03-24 04:13:33 +0300

    created image component in Post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
+import Image from './Image';
 
 const Post = ({ content }) => {
 	return (
-		<div className='rounded col-span-4 border bg-white border-gray-primary mb-16'>
+		<div className='rounded col-span-4 border bg-white border-gray-primary mb-8'>
 			<Header username={content.username} />
+			<Image src={content.imageSrc} caption={content.caption} />
 		</div>
 	);
 };
