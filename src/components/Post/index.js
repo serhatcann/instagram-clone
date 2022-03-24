c6a96078c00c1fa4d4791118109830873f73f64f
Serhat Can <serhatcan1996@gmail.com>
2022-03-24 05:12:46 +0300

    Created actions and footer components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,12 +2,26 @@ import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Image from './Image';
-
+import Actions from './Actions';
+import Footer from './Footer';
 const Post = ({ content }) => {
+	const commentInput = useRef(null);
+
+	const handleFocus = () => {
+		commentInput.current.focus();
+	};
+
 	return (
 		<div className='rounded col-span-4 border bg-white border-gray-primary mb-8'>
 			<Header username={content.username} />
 			<Image src={content.imageSrc} caption={content.caption} />
+			<Actions
+				docId={content.docId}
+				totalLikes={content.likes.length}
+				likedPhoto={content.userLikedPhoto}
+				handleFocus={handleFocus}
+			/>
+			<Footer caption={content.caption} username={content.username} />
 		</div>
 	);
 };
