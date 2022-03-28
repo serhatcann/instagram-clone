0de8ba44ce043f06ce995ea983bb09ea0be744cf
Serhat Can <serhatcan1996@gmail.com>
2022-03-28 05:51:46 +0300

    fixed the profile redirection not working bug on Post header name/img click in dashboard

--- src/components/Post/Header.js
+++ src/components/Post/Header.js
@@ -5,13 +5,14 @@ const Header = ({ username }) => {
 	return (
 		<div className='flex border-b border-gray-primary h-4 p-4 py-8'>
 			<div className='flex items-center'>
-				<Link to={`/p/${username}`} className='flex items-center ' />
-				<img
-					src={`/images/avatars/${username}.jpg`}
-					alt={`${username} profile`}
-					className='rounded-full h-8 w-8 flex mr-3'
-				/>
-				<p className='font-bold'>{username}</p>
+				<Link to={`/p/${username}`} className='flex items-center '>
+					<img
+						src={`/images/avatars/${username}.jpg`}
+						alt={`${username} profile`}
+						className='rounded-full h-8 w-8 flex mr-3'
+					/>
+					<p className='font-bold'>{username}</p>
+				</Link>
 			</div>
 		</div>
 	);
